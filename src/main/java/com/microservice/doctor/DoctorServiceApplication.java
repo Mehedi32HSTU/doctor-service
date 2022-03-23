ffee15f4a88cfa41029d94ccbd4ecc470872aaac
Mehedi32HSTU <mehedi32.cse.hstu@gmail.com>
2022-03-24 00:23:44 +0600

    Additional route added on main class for testing

--- src/main/java/com/microservice/doctor/DoctorServiceApplication.java
+++ src/main/java/com/microservice/doctor/DoctorServiceApplication.java
@@ -1,10 +1,21 @@
 package com.microservice.doctor;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DoctorServiceApplication {
+	
+	@GetMapping("/")
+	public ResponseEntity<String> sayHello(){
+		return ResponseEntity.ok().body("DoctorServiceApplication is Running. Current server time : " + new Date().toString());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorServiceApplication.class, args);
